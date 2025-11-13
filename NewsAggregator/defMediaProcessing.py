180710f458312935778c633730f676c74d4db13d
Иван Васильев <balamout75@gmail.com>
2025-11-13 15:55:55 +0800

    ready

--- NewsAggregator/defMediaProcessing.py
+++ NewsAggregator/defMediaProcessing.py
@@ -1,4 +1,6 @@
-import textwrap
+﻿import textwrap
+import articleDateExtractor
+
 from grab import Grab
 from bs4 import BeautifulSoup as bs
 from datetime import datetime, timedelta
@@ -8,15 +10,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def getArticleDate(source_name, article):
-    if source_name == 'dvnovosti.ru':
-        time1 = article.find('pubDate')
-        print(time1)
-        ArticleDate = datetime.strptime(time1.text[:16], '%a, %d %b %Y').strftime('%Y-%m-%d')
-        return ArticleDate
+import locale
+locale.setlocale(locale.LC_ALL, "")
 
 
-def getMedia(source = list()):
+def getMedia(source, deep):
     url = source['url']
     prefix = source['prefix']
     articles = []
@@ -34,7 +32,7 @@ def getMedia(source = list()):
         else: mainUrl = url
         """ Формирование списка обработанных сообщений СМИ за текущий день """
         try:
-            processedMediaLinksFile = open(prefix + '/' + str(datetime.today().strftime('%Y-%m-%d')) + ' Processed Articles Links.txt', mode='r', encoding='utf-8')
+            processedMediaLinksFile = open(prefix + '/' + str((datetime.today() - timedelta(days=deep)).strftime('%Y-%m-%d')) + ' Processed Articles Links.txt', mode='r', encoding='utf-8')
             processedLinks = processedMediaLinksFile.read().split()
             processedMediaLinksFile.close()
         except Exception:
@@ -51,21 +49,26 @@ def getMedia(source = list()):
         """ Поиск ссылок на сообщения СМИ за текущий день с учетом обработанных """
         try:
             articlesBox = soup.select(source['articlesBox']['articleSelector'])
+            """"""
         except Exception as e:
             errorFile.write(str(datetime.today().strftime('%Y-%m-%d %H:%M')) + ' #' + str(errorNumber) + ' Source: ' + mainUrl + ', ' + 'Error: ' + str(e) + '\n')
             continue
-
+        i=0
+        articlesdate = soup.select('div.top-line span')
         for article in articlesBox:
-            """ Обработка сообщений из новостной ленты сайта """
-            href = article.find('guid').text.strip()
+            """ Обработка сообщений из новостной ленты сайта"""
+            href=article['href']
+
             if 'http' != href[:4]: href = url + href
             if href not in processedLinks:
-                #if getArticleDate(prefix, article) == datetime.today().strftime('%Y-%m-%d'):
-                if getArticleDate(prefix, article) == (datetime.today()-timedelta(days=0)).strftime('%Y-%m-%d'):
-                    """ Получение содержания сообщения СМИ """
+
+                d1 = datetime.strptime(articlesdate[i].text, '%d.%m.%Y').strftime('%Y-%m-%d')
+                i += 2
+                if d1 == (datetime.today()-timedelta(days=deep)).strftime('%Y-%m-%d'):
+                    """  Получение содержания сообщения СМИ """
                     try:
                         browser.get(href)
-                        sleep(10)
+                        #sleep(10)
                         generated_html = browser.page_source
                         articleSoup = bs(generated_html, 'lxml')
                     except Exception as e:
@@ -73,26 +76,48 @@ def getMedia(source = list()):
                         errorFile.write(str(datetime.today().strftime('%Y-%m-%d %H:%M')) + ' #' + str(errorNumber) + ' Source: ' + href + ', ' + 'Error: ' + str(e) + '\n')
                         errorNumber += 1
                         continue
-                    articleParams = '/СМИ\n'
-                    articleParams += '1/' + source['id'] + '\n'
-                    articleParams += '2/' + str((datetime.today()-timedelta(days=0)).strftime('%Y-%m-%d')) + '\n'
-                    articleParams += '3/\n'
-                    title = articleSoup.select_one('title')
-                    articleParams += '4/' + title.text.strip() + '\n'
-                    f = articleSoup.select('p')
+                    articleParams = 'Header\n'
+                    articleParams += '*/' + source['id'] + '\n'
+                    articleParams += '*/' + str(
+                        ((datetime.today() - timedelta(days=deep))).strftime('%Y-%m-%d')) + '\n'
+
+                    title=''
+                    articleParams += '*/\n'
+
+                    title = articleSoup.select_one('h3')
+                    titlee=title.text.strip()
+
+
+                    articleParams += '*' + titlee + '\n'
+                    f = articleSoup.select('div.news-detail p')
                     ThisText=''
                     for abz in f:
                         ThisText+= abz.text.strip()+'\n'
-                    articleParams += '5/' + ThisText
+                    articleParams += '*/' + ThisText
+                    #tab
+                    while '\t' in articleParams: articleParams = articleParams.replace('\t', ' ')
+                    #probel
                     while '  ' in articleParams: articleParams=articleParams.replace('  ',' ')
+                    #enter
+                    while '\n\n' in articleParams: articleParams = articleParams.replace('\n\n', '\n')
+
                     articleParams = articleParams.replace('Ё', 'Е').replace('ё', 'е')
+                    articleParams = articleParams.replace('«', '"').replace('»', '"')
+
                     """ Запись обработанного сообщения СМИ в список """
                     articles.append(articleParams)
                     """ Запись ссылки сообщения СМИ в список и файл обработанных """
                     processedLinks.append(href)
-                    processedMediaLinksFile = open(prefix + '/' + str(datetime.today().strftime('%Y-%m-%d')) + ' Processed Articles Links.txt',  mode='a', encoding='utf-8')
+                    processedMediaLinksFile = open(prefix + '/' + str(
+                        (datetime.today() - timedelta(days=deep)).strftime(
+                            '%Y-%m-%d')) + ' Processed Articles Links.txt', mode='a', encoding='utf-8')
+
                     processedMediaLinksFile.write(href + ' ')
                     processedMediaLinksFile.close()
+                    """если начался новый день"""
+
+
+
     """ Выгрузка всех обработанных сообщений СМИ из функции"""
     browser.quit()
     errorFile.close()
--- NewsAggregator/getDVHubDaily.py
+++ NewsAggregator/getDVHubDaily.py
@@ -1,4 +1,4 @@
-﻿from os import mkdir, getcwd, path, rename, replace
+from os import mkdir, getcwd, path, rename, replace
 from datetime import datetime, timedelta
 import defMediaProcessing as dmp
 import json, time
@@ -7,17 +7,17 @@ import sys
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-
 start_time = time.time()
 
-
+"""with open('sources.json', 'r', encoding='utf-8') as fh:
+    sources = json.load(fh)"""
 sources = [
     {'id': '1',
-     'url': 'https://www.khabarovskadm.ru',
-     'prefix': 'khabarovskadm.ru',
-     'parser': 'lxml',
-     'paths': ['/news/index.php'],
-     'articlesBox': {'articleSelector': 'a.list-title',
+     'url': 'https://www.dvnovosti.ru',
+     'prefix': 'dvnovosti.ru',
+     'parser': 'xml',
+     'paths': ['/rss/'],
+     'articlesBox': {'articleSelector': 'item',
                      'anotherArticleSelector': ''},
      'articleContent': {'tag': 'div',
                         'attr': 'class',
@@ -27,8 +27,10 @@ sources = [
      }
  ]
 
+#with open('sources123.json', 'w', encoding='utf-8') as fh:
+#    json.dump(sources, fh)
 
-try:
+try :
     if len(sys.argv) > 1:
         deepstr=sys.argv[1]
         if (deepstr.isdigit()): deep = int(deepstr)
@@ -37,10 +39,9 @@ except Exception:
     print('нет аргументов' + Exception)
 
 todayMediaStat = open(str(datetime.today().strftime('%Y-%m-%d')) + ' Media statistics.txt', mode='a', encoding='utf-8')
-if path.isfile('hb' + str((datetime.today() - timedelta(days=1)).strftime('%d%m%y')) + '_khabarovskadm.smi'):
-    rename('hb' + str((datetime.today() - timedelta(days=1)).strftime('%d%m%y')) + '_khabarovskadm.smi', 'hb' +
-           str(datetime.today().strftime('%d%m%y')) + '_khabarovskadm.smi')
-todayMediaFile = open('hb' + str(datetime.today().strftime('%d%m%y_%H_%M')) + '_khabarovskadm.smi', mode='a', encoding='cp866')
+if path.isfile('hb' + str((datetime.today() - timedelta(days=1)).strftime('%d%m%y')) + '_dvnovosti.smi'):
+    rename('hb' + str((datetime.today() - timedelta(days=1)).strftime('%d%m%y')) + '_dvnovosti.smi', 'hb' + str(datetime.today().strftime('%d%m%y')) + '_dvnovosti.smi')
+todayMediaFile = open('hb' + str(datetime.today().strftime('%d%m%y_%H_%M')) + '_dvnovosti.smi', mode='a', encoding='cp866')
 
 
 totalHourMedia = 0
@@ -52,19 +53,14 @@ for source in sources:
         mkdir(getcwd() + '/' + source['prefix'])
     for article in dmp.getMedia(source, deep):
         todayMediaFile.write(str(article.encode('cp866', 'ignore').decode('cp866')) + '\n')
-
         sourceMediaCount += 1
-    if sourceMediaCount > 0:
-        todayMediaStat.write(str(datetime.today().strftime('%Y-%m-%d %H:%M')) + ' С сайта ' + source['prefix'] +
-                             ' загружено сообщений: ' + str(sourceMediaCount) + ' (затрачено: {:.2f} сек.)'.format(time.time() -
-                                                                                                                   start_time_source) + '\n')
+`    if sourceMediaCount > 0:
+        todayMediaStat.write(str(datetime.today().strftime('%Y-%m-%d %H:%M')) + ' С сайта ' + source['prefix'] + ' загружено сообщений: ' + str(sourceMediaCount) + ' (затрачено: {:.2f} сек.)'.format(time.time() - start_time_source) + '\n')
     totalHourMedia += sourceMediaCount
 
-todayMediaStat.write(str(datetime.today().strftime('%Y-%m-%d %H:%M')) +
-                     ' Со всех обрабатываемых источников загружено: <' + str(totalHourMedia) + '>\n')
+todayMediaStat.write(str(datetime.today().strftime('%Y-%m-%d %H:%M')) + ' Со всех обрабатываемых источников загружено: <' + str(totalHourMedia) + '>\n')
 
 todayMediaStat.close()
-
 filename = todayMediaFile.name
 todayMediaFile.close()
 
@@ -77,13 +73,13 @@ try:
     MMT=MIMEText(myfile.read())
     MMT.add_header('Content-Disposition', 'attachment', filename=filename)
     msg.attach(MMT)
-    msg['Subject'] = 'news'
+    msg['Subject'] = 'DV_News'
     msg['From'] = sender_email
     msg['To'] = receiver_email
     smtp = smtplib.SMTP('smtp.mail.ru',25)
     smtp.ehlo()
     smtp.starttls()
-    smtp.login(sender_email, 'pass')
+    smtp.login(sender_email, '')
     smtp.sendmail(sender_email,receiver_email, msg.as_string())
     smtp.quit()
     myfile.close()
@@ -92,20 +88,20 @@ except Exception:
     print('ОШИБКА ОТПРАВКИ ПОЧТЫ 1'+Exception)
 try:
     sender_email = 'sender@mail.ru'
-    receiver_email = 'recipient2@mail.ru'
+    receiver_email = 'recipient@mail.ru'
     msg = MIMEMultipart()
     msg.attach(MIMEText('С Уважением!'))
     myfile=open(filename, mode='r', encoding='cp866')
     MMT=MIMEText(myfile.read())
     MMT.add_header('Content-Disposition', 'attachment', filename=filename)
     msg.attach(MMT)
-    msg['Subject'] = 'news'
+    msg['Subject'] = 'DV_News'
     msg['From'] = sender_email
     msg['To'] = receiver_email
     smtp = smtplib.SMTP('smtp.mail.ru',25)
     smtp.ehlo()
     smtp.starttls()
-    smtp.login(sender_email, 'pass')
+    smtp.login(sender_email, '')
     smtp.sendmail(sender_email,receiver_email, msg.as_string())
     smtp.quit()
     myfile.close()
@@ -114,20 +110,20 @@ except Exception:
     print('ОШИБКА ОТПРАВКИ ПОЧТЫ 2'+Exception)
 try:
     sender_email = 'sender@mail.ru'
-    receiver_email = 'recipient2@mail.ru'
+    receiver_email = 'recipient@mail.ru'
     msg = MIMEMultipart()
     msg.attach(MIMEText('С Уважением!'))
     myfile=open(filename, mode='r', encoding='cp866')
     MMT=MIMEText(myfile.read())
     MMT.add_header('Content-Disposition', 'attachment', filename=filename)
     msg.attach(MMT)
-    msg['Subject'] = 'news'
+    msg['Subject'] = 'DV_News'
     msg['From'] = sender_email
     msg['To'] = receiver_email
     smtp = smtplib.SMTP('smtp.mail.ru',25)
     smtp.ehlo()
     smtp.starttls()
-    smtp.login(sender_email, 'pass')
+    smtp.login(sender_email, '')
     smtp.sendmail(sender_email,receiver_email, msg.as_string())
     smtp.quit()
     myfile.close()
